9131e5c83ea9875134c421ce83f5c23714e35a04
Durvesh7k <dschopade7k@gmail.com>
2023-07-23 12:29:22 +0530

    update the url

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from '../constants/config';
 import { getAccessToken, getType } from '../utils/common-utils';
 
-const API_URL = ""
+const API_URL = "https://insightlnk.onrender.com"
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
